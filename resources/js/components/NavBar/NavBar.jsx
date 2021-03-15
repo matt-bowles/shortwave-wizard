99fbd95d344ad5d6f720d8219cb849380e2d94d3
matt-bowles <mattbowles1337@gmail.com>
2021-03-16 09:37:04 +1000

    Temporary navbar

--- resources/js/components/NavBar/NavBar.jsx
+++ resources/js/components/NavBar/NavBar.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
-import { AppBar } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
 import styles from './NavBar.module.css';
 
 export default class Title extends Component {
     render() {
         return (
-            <nav>
-                <Link to="/"><h1>Shortwave Wizard 🧙</h1></Link>
-                <Link to="/about"><li>About</li></Link>
-            </nav>
+
+            <AppBar position="static">
+            <Toolbar style={{color: "white"}}>
+                
+                <Button component={Link} to={"/"} style={{marginRight: "60px"}}>Shortwave Wizard</Button>
+ 
+                <Button component={Link} to={"/about"} color="inherit" style={{float: "right"}}>About</Button>
+            </Toolbar>
+            </AppBar>
+
+            // <nav>
+            //     <Link to="/"><h1>Shortwave Wizard 🧙</h1></Link>
+            //     <Link to="/about"><li>About</li></Link>
+            // </nav>
         )
     }
 }
